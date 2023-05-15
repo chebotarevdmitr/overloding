c3a2cbf2b8e3d19a47f41524844f769cafff17fc
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-15 22:58:03 +0300

    add function

--- overloding.cpp
+++ overloding.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+int sum(int a,int b);
+double sum(double a,double b);
+float sum(float a,float b, float c);
 int main() {
-	cout<<"Hello overloding\n";
+	
 	
 	system("pause>0");
+}
+// int sum(int a,int b){
+// int result = a+b;
+//   return result;
+
+int sum(int a,int b){
+	return a+b;
+}
+
+double sum(double a,double b){
+	return a+b;
+}
+float sum(float a,float b, float c){
+	return a+b+c;
 }
